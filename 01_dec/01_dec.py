275bcae539c196fbacd7361b076717b63e596576
emscherl <martin.scherl5@gmail.com>
2020-12-03 16:10:26 +0100

    task 2

--- 01_dec/01_dec.py
+++ 01_dec/01_dec.py
@@ -2,10 +2,12 @@ f = list(map(int, open('data')))
 
 for i in f:
     for j in f:
-        if i + j == 2020:
-            print(i)
-            print(j)
-            print(i * j)
+        for k in f:
+            if i + j + k == 2020:
+                print(i)
+                print(j)
+                print(k)
+                print(i * j * k)
 
 # print ([i * j for i in f for j in f if i + j == 2020])
 
