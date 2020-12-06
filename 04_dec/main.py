0858e4b1eac5ebc4690d9d2e022b5e05f9bff802
emscherl <martin.scherl5@gmail.com>
2020-12-06 22:54:12 +0100

    part two done

--- 04_dec/main.py
+++ 04_dec/main.py
@@ -1,14 +1,25 @@
+import re
+
 with open('data.txt') as f:
     passports_ugly = f.read().split('\n\n')
 
+
 def height_checker(s):
     if s[-2:] == 'cm':
-        if 150 <= int(s[:2]) <= 193:
+        try:
+            h = int(s[:3])
+        except ValueError:
+            h = int(s[:2])
+        if 150 <= h <= 193:
             return True
         else:
             return False
     elif s[-2:] == 'in':
-        if 59 <= int(s[:1]) <= 76:
+        try:
+            h = int(s[:2])
+        except ValueError:
+            h = int(s[:1])
+        if 59 <= h <= 76:
             return True
         else:
             return False
@@ -19,8 +30,11 @@ def height_checker(s):
 values = {'byr': lambda s: len(s) == 4 and 1920 <= int(s) <= 2002,
           'iyr': lambda s: len(s) == 4 and 2010 <= int(s) <= 2020,
           'eyr': lambda s: len(s) == 4 and 2020 <= int(s) <= 2030,
-          'hgt': height_checker, 'hcl', 'ecl', 'pid']
-
+          'hgt': height_checker,
+          'hcl': lambda s: re.match(r'#[a-f0-9]{6}', s),
+          'ecl': lambda s: s in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
+          'pid': lambda s: len(s) == 9 and s.isdigit()
+          }
 
 passports = []
 
@@ -28,10 +42,19 @@ for p in passports_ugly:
     passports.append(p.replace('\n', ' ').split(' '))
 
 counter = 0
+counter_call2 = 0
 
 for p in passports:
     d = dict(i.split(':') for i in p)
     if all(v in d for v in values):
         counter += 1
+        checker = True
+        for i in values:
+            fun = values[i]
+            if not fun(d[i]):
+                checker = False
+        if checker:
+            counter_call2 += 1
 
 print(counter)
+print(counter_call2)
