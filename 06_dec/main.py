6090bf03d620424288d5d60dcebd14441185c54d
emscherl <martin.scherl5@gmail.com>
2020-12-07 11:56:25 +0100

    add solution for part 2

--- 06_dec/main.py
+++ 06_dec/main.py
@@ -4,10 +4,22 @@ with open("data.txt") as f:
 g_answers = []
 counter = 0
 
-for g in g_answers_ugly:
-    g_answers.append(g.replace('\n', ''))
 
-for g in g_answers:
-    counter += (len(set(g)))
+for g in g_answers_ugly:
+    g_answers.append(g.split('\n'))
+
+# print(g_answers)
+
+for group in g_answers:
+    counter_p = 0
+    if len(group) == 1:
+        counter += len(group[0])
+    else:
+        counter_p = len(group)
+        group = ''.join(group)
+        for i in set(group):
+            if group.count(i) == counter_p:
+                counter += 1
+
 
 print(counter)
