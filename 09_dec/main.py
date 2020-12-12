be0e577d57de9918eab563cb0b6c7570477c48fa
emscherl <martin.scherl5@gmail.com>
2020-12-13 00:03:16 +0100

    part 2 done

--- 09_dec/main.py
+++ 09_dec/main.py
@@ -1,12 +1,15 @@
 with open('data.txt') as f:
     xmas = [int(i) for i in f]
 
+lst_elem = 25
+inv_num = int
 
-def get_prev_5(idx: int, lst: list) -> list:
-    return lst[idx - 25:idx]
+
+def get_prev(idx: int, lst: list, num_elem: int) -> list:
+    return lst[idx - num_elem:idx]
 
 
-def check_sum(num: int, lst: list):
+def check_sum(num: int, lst: list) -> bool:
     checklist = []
     for i in range(len(lst)):
         for x in lst:
@@ -19,9 +22,26 @@ def check_sum(num: int, lst: list):
 
 
 for i in range(len(xmas)):
-    if i < 25:
+    if i < lst_elem:
         continue
-    prev_5 = get_prev_5(i, xmas)
+    prev_5 = get_prev(i, xmas, lst_elem)
     if not check_sum(xmas[i], prev_5):
+        inv_num = xmas[i]
         print(xmas[i], 'at idx', i)
         break
+
+
+def get_en_weak(lst: list, num: int) -> list:
+    for i in range(len(lst)):
+        en_weak = [lst[i]]
+        for e in range(1, len(lst)):
+            if i + e == len(lst):
+                break
+            en_weak.append(lst[i + e])
+            if sum(en_weak) == num:
+                return en_weak
+
+
+en_weak_lst = get_en_weak(xmas, inv_num)
+
+print('contiguous set: ', en_weak_lst, ' Solution: ', min(en_weak_lst) + max(en_weak_lst))
